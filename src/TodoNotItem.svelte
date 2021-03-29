47bdd0d8d33fe6aa183f588c0007183127bcdcb5
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-03-29 19:06:28 +0200

    item structure complete

---
+++ src/TodoNotItem.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import { fly } from "svelte/transition";
+</script>
+
+<div class="todo-not-item">
+  <div class="todo-not-item-left" transition:fly={{ y: 20, duration: 300 }}>
+    <input
+      type="checkbox"
+      bind:checked={completed}
+      on:change={toggleCompleted}
+    />
+    <div class="todo-not-item-label" class:completed>{title}</div>
+  </div>
+  <div class="remove-item" on:click={deleteTodoNot}>x</div>
+</div>
